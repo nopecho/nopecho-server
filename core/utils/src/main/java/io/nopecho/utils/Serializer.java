a4edacd5d665bb99fc86b4db47331fb2d3652240
nopecho <ch.jooon@gmail.com>
2023-06-13 01:11:04 +0900

    feat: utils object to map

--- core/utils/src/main/java/io/nopecho/utils/Serializer.java
+++ core/utils/src/main/java/io/nopecho/utils/Serializer.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import java.util.Map;
+
 public class Serializer {
 
     private static volatile ObjectMapper mapper;
@@ -34,6 +36,15 @@ public class Serializer {
     public static <T> T deserialize(String target, Class<T> clazz) {
         try {
             return get().readValue(target, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static Map<String, Object> convertMap(Object o) {
+        String serialize = serialize(o);
+        try {
+            return get().convertValue(o, Map.class);
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
